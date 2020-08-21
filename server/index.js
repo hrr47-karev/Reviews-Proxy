b2a73af01e87241a9f2b420d2099972dcc2139fd
Ben Chasin <benachasin@gmail.com>
2020-08-21 16:19:36 -0700

    deployment 1

--- server/index.js
+++ server/index.js
@@ -3,13 +3,14 @@ const path = require('path');
 const app = express();
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-app.use('/api/reviews', createProxyMiddleware({ target: 'http://localhost:3001' }));
+app.use('/hostels/:id/api/reviews', createProxyMiddleware({ target: 'http://localhost:3001' }));
 app.use('/api/hostel/1/rooms', createProxyMiddleware({ target: 'http://localhost:3009' }));
-app.use('/hostels/:id/hostel', createProxyMiddleware({ target: 'http://localhost:3000' }));
-app.use('/hostels/:id/address', createProxyMiddleware({ target: 'http://localhost:3000' }));
-app.use('/hostels/:id/description', createProxyMiddleware({ target: 'http://localhost:3000' }));
-app.use('/hostels/:id/rules', createProxyMiddleware({ target: 'http://localhost:3000' }));
+app.use('api/hostels/:id/hostel', createProxyMiddleware({ target: 'http://localhost:3000' }));
+app.use('api/hostels/:id/address', createProxyMiddleware({ target: 'http://localhost:3000' }));
+app.use('api/hostels/:id/description', createProxyMiddleware({ target: 'http://localhost:3000' }));
+app.use('api/hostels/:id/rules', createProxyMiddleware({ target: 'http://localhost:3000' }));
 
+app.use('/api/hostels/:id/images', createProxyMiddleware({ target: 'http://localhost:3007'});
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
